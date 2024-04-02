5806a235d5a17fdd33484812580436ca2924b59a
omkar <omkar.c.344@gmail.com>
2024-04-03 02:35:09 +0530

    modified config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,20 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config({ path: "../.env" });
 require("@nomiclabs/hardhat-etherscan");
 
+const { SEPOLIA_URL, PRIVATE_KEY_1, PRIVATE_KEY_2, ETHERSCAN_API_KEY } =
+  process.env;
+
 module.exports = {
   solidity: "0.8.24",
+
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [`0x${PRIVATE_KEY_1}`, `0x${PRIVATE_KEY_2}`],
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
